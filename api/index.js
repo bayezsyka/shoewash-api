a3e2c4e5475ea324d42f6e2a266474750b386c57
bayezsyka <farrosy6@gmail.com>
2025-10-23 00:23:38 +0700

    bismillah

--- api/index.js
+++ api/index.js
@@ -1,4 +1,9 @@
-const serverless = require('serverless-http');
-const app = require('../src/app');
+// api/index.js
+const app = require("../src/app");
 
-module.exports = serverless(app);
+// Vercel cukup butuh handler (req, res) standar Node/Express
+module.exports = (req, res) => {
+  // optional: handle HEAD ping cepat
+  if (req.method === "HEAD") return res.status(200).end();
+  return app(req, res);
+};
